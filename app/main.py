51ed548f0386736c1098cf4f78180d6a911f5f2b
Mohammed Abuelwafa <Mohammed_Abuelwafa@mentor.com>
2023-03-27 02:07:47 +0200

    encoded PONG as binary string instead of f string

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_connection , _ = server_socket.accept() # wait for client
     client_connection.recv(1024)  # wait for client to send data
-    client_connection.send(f"+PONG\r\n")
-    print("SENDING PONG!")
+    client_connection.send(b"+PONG\r\n")
 
 
 
